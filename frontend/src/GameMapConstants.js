ea611e3f15eebfc9b854dae519ccefff982205a5
radomirKrastev <49518363+radomirKrastev@users.noreply.github.com>
2020-11-01 17:27:30 +0200

    add game map constants

---
+++ frontend/src/GameMapConstants.js
@@ -0,0 +1,7 @@
+import { point, layout, flatLayout } from './hexagonMathFunctions';
+
+// point args = widht, height
+export const HEX_SIZE = point(40, 40);
+export const ORIGIN_POSITION = point(500, 500);
+export const SCREEN_LAYOUT = layout(flatLayout, HEX_SIZE, ORIGIN_POSITION);
+export const RADIUS_SIZE = 2;
