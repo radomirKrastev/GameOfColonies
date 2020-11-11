a863b0b74b059f338267d52812254c6da9fc8c9f
radomirKrastev <49518363+radomirKrastev@users.noreply.github.com>
2020-11-11 21:59:37 +0200

    replace some of the images

--- frontend/src/images.js
+++ frontend/src/images.js
@@ -26,21 +26,21 @@
 const width = 150;
 const height = 100;
 
-const images = [
-    `https://images.unsplash.com/photo-1530799750-13f7b374d113?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
-    `https://images.unsplash.com/photo-1530483669142-1c576f37c37b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
-    `https://images.unsplash.com/photo-1599389932245-f414c9576ab4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
-    `https://images.unsplash.com/photo-1535093838480-90f893310529?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
-    `https://images.unsplash.com/photo-1562060879-cbdb5285a6de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
-    `https://images.unsplash.com/photo-1593980747575-c05c2f0b0dfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
-];
+// const images = [
+//     `https://images.unsplash.com/photo-1530799750-13f7b374d113?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
+//     `https://images.unsplash.com/photo-1530483669142-1c576f37c37b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
+//     `https://images.unsplash.com/photo-1599389932245-f414c9576ab4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
+//     `https://images.unsplash.com/photo-1535093838480-90f893310529?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
+//     `https://images.unsplash.com/photo-1562060879-cbdb5285a6de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
+//     `https://images.unsplash.com/photo-1593980747575-c05c2f0b0dfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`,
+// ];
 
 const wheatTiles = Array(4).fill(`https://images.unsplash.com/photo-1562060879-cbdb5285a6de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`);
-const forestTiles = Array(4).fill(`https://images.unsplash.com/photo-1530799750-13f7b374d113?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`);
-const sheepTiles = Array(4).fill(`https://images.unsplash.com/photo-1530483669142-1c576f37c37b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`);
-const stoneTiles = Array(3).fill(`https://images.unsplash.com/photo-1599389932245-f414c9576ab4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`);
+const forestTiles = Array(4).fill(`https://images.unsplash.com/photo-1500380176164-2a1f4445e086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`);
+const sheepTiles = Array(4).fill(`https://images.unsplash.com/photo-1585924966618-13365f890131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`);
+const stoneTiles = Array(3).fill(`https://images.unsplash.com/photo-1513083457273-8ea7b2a045c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`);
 const brickTiles = Array(3).fill(`https://images.unsplash.com/photo-1593980747575-c05c2f0b0dfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`);
-const dezertTile = [`https://images.unsplash.com/photo-1535093838480-90f893310529?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`];
+const dezertTile = [`https://images.unsplash.com/photo-1488197047962-b48492212cda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=${width}&q=${height}`];
 
 export let mapTilesBackground = [
     ...wheatTiles,
