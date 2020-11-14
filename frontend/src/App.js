72ba7325357a26297b3498ba68110b3e135d6eab
radomirKrastev <49518363+radomirKrastev@users.noreply.github.com>
2020-11-14 14:32:44 +0200

    Pressing a button should show/hide
    settlement spots #1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import GameMap from './GameMap';
-import ActionComponent from './ActionComponent';
+import SettlementSetups from './SettlementSetups';
 
 import './App.scss';
 
 const App = () => (
   <div className="app-wrapper">
     <GameMap></GameMap>
-    <ActionComponent></ActionComponent>
   </div>
 );
 
--- frontend/src/GameMap.js
+++ frontend/src/GameMap.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { Stage } from '@inlet/react-pixi';
 
 import { hex, polygonCorners, generateHexagonalGrid } from './hexagonMathFunctions';
 import { SCREEN_LAYOUT, RADIUS_SIZE } from './GameMapConstants';
 
 import Hexagon from './Hexagon';
+import SettlementSetups from './SettlementSetups';
 
-import './App.css';
 import './GameMap.scss';
 
 let gridHexesCoordinatesMap = generateHexagonalGrid(RADIUS_SIZE);
@@ -17,13 +18,25 @@ gridHexesCoordinatesMap.slice().forEach(hexCoordinates => {
     gridHexesCornersMap.push(polygonCorners(SCREEN_LAYOUT, hex(hexCoordinates.q, hexCoordinates.r, hexCoordinates.s)));
 });
 
+// console.log(gridHexesCornersMap);
+
 const HexagonGrid = () => {
+    const [mapTargetsVisible, setMapTargetsVisible] = useState(false);
+
+    const toggleSettlementTargetsHandler = () => {
+        setMapTargetsVisible(!mapTargetsVisible);
+    }
+
     return <div className="game-map-wrapper">
         <Stage className="game-map-stage" width={1000} height={470} options={{ antialias: true, backgroundColor: 0xeef1f5 }}>
             {gridHexesCornersMap.map((x, i) => {
                 return <Hexagon key={i} hexagonCorners={x}></Hexagon>
             })}
+            {mapTargetsVisible ? <SettlementSetups gridHexesCornersMap={gridHexesCornersMap}></SettlementSetups> : null}
         </Stage>
+        <div className="show-settlement-targets">
+            <button onClick={() => toggleSettlementTargetsHandler()}>TEST Button</button>
+        </div>
     </div>
 };
 
