407f0f5db34285faa8dbb7eaf21abdd8735205f5
radomirKrastev <49518363+radomirKrastev@users.noreply.github.com>
2020-11-16 22:18:31 +0200

    clear commented code
    #3

--- server/app.js
+++ server/app.js
@@ -1,41 +1,21 @@
-// import './database/db';
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const cors = require('cors');
-let { DB_URL, API_PORT } = require('./envConstants');
-
-// import { socker } from './socker';
-// import { handleError, authenticated, logger } from './middlewares';
-// import { API_PORT, hosts } from './env';
-
+let { API_PORT } = require('./envConstants');
 
 const app = express();
 const server = new http.Server(app);
-// socker(server);
 
-// app.use(cors({ origin: hosts, credentials: true }));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use('/users', authenticated);
-// app.use('/search', authenticated);
 
 app.use(routes);
-
-// app.use((err, _req, res, _) => {
-//     handleError(err, res);
-// });
 
 console.log("asd", API_PORT);
 
 app.listen(API_PORT, () => {
-    // console.log(`Api listening on port ${Number(API_PORT)}!`);
     console.log(`Api listening on port ${API_PORT}!`);
 });
-
-// server.listen(Number(API_PORT) + 1, () => {
-//     logger.info(`Socker listening on port ${Number(API_PORT) + 1}!`);
-//     // logger.info(`Api and socker whitelisted for ${hosts}`);
-// });
--- server/envConstants.js
+++ server/envConstants.js
@@ -1,42 +1,11 @@
-// mongo connection string
-//mongodb+srv://radomir:<password>@cluster0.17orc.mongodb.net/<dbname>?retryWrites=true&w=majority
-
 const dotenv = require('dotenv');
 dotenv.config();
 
 const {
-    // ALGOLIA_ID,
-    // ALGOLIA_SEARCH_API_KEY,
-    // ALGOLIA_INDEX_NAME,
     DB_NAME,
     MONGO_PASSWORD,
-    API_PORT,
-    // API_KEY,
-    // SALT_ROUNDS
+    API_PORT
 } = process.env;
 
-
-
-
-// export const DB_URL = `mongodb+srv://saurav:${MONGO_PASSWORD}@fifa.ejduy.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 exports.DB_URL = `mongodb+srv://radomir:${MONGO_PASSWORD}@cluster0.17orc.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-exports.API_PORT = process.env.API_PORT;
-// export const MAX_TIMER_DEFAULT = 120 * 1000;
-// export const MAX_PLAYERS_DEFAULT = 14;
-
-// export let hosts = [];
-// if (process.env.NODE_ENV === 'production') {
-//     hosts = ['https://fifa.sauravmh.com', 'https://sauravmh.vercel.app'];
-// } else {
-//     hosts = ['http://localhost:3000'];
-// }
-
-// export const ROOM_ID_RX = /^([A-Z0-9]){6}$/;
-
-// export const ATTRIBUTES_TO_RETRIEVE = ['name', 'positions', 'Overall Rating', 'Skill Moves', 'objectID', 'photo_url'];
-
-// module.exports = {DB_NAME,
-//     MONGO_PASSWORD,
-//     API_PORT, DB_URL};
-
-// module.exports = DB_URL;
+exports.API_PORT = API_PORT;
