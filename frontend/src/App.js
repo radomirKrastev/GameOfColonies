075568a2e0bbbc5ea045cdcd355025917ee2df35
radomirKrastev <49518363+radomirKrastev@users.noreply.github.com>
2020-11-29 18:21:28 +0200

    create socket connection between
    client and server #2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import GameMap from './GameMap';
 import SettlementSetups from './SettlementSetups';
+import io from 'socket.io-client';
 
 import './App.scss';
 
-const App = () => (
-  <div className="app-wrapper">
+const App = () => {
+  const socket = io('http://localhost:5001');
+
+  return ( <div className="app-wrapper">
     <GameMap></GameMap>
-  </div>
-);
+  </div> )
+};
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const socketIo = require('socket.io');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const cors = require('cors');
@@ -7,6 +8,11 @@ let { API_PORT } = require('./envConstants');
 
 const app = express();
 const server = new http.Server(app);
+const io = socketIo(server, {
+    cors: {
+        origin: '*',
+    }
+});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,8 +20,17 @@ app.use(bodyParser.json());
 
 app.use(routes);
 
-console.log("asd", API_PORT);
+io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
+    console.log('new client connected');
+    socket.emit('connection', null);
+});
 
 app.listen(API_PORT, () => {
     console.log(`Api listening on port ${API_PORT}!`);
+});
+
+server.listen(Number(API_PORT) + 1, () => {
+    console.log(`SocketIo listening on port ${Number(API_PORT) + 1}!`);
+    // logger.info(`Socker listening on port ${API_PORT + 1}!`);
+    // logger.info(`Api and socker whitelisted for ${hosts}`);
 });
