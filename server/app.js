262ec5d177db3ec090083f7b8bdad13910d9a2a1
radomirKrastev <49518363+radomirKrastev@users.noreply.github.com>
2020-12-06 14:34:19 +0200

    generate mapTiles from BE #2

--- server/app.js
+++ server/app.js
@@ -6,12 +6,17 @@ const routes = require('./routes');
 const cors = require('cors');
 let { API_PORT } = require('./envConstants');
 
+const { mapTilesBackground } = require('./images');
+const { shuffleArray } = require('./utils');
+const mapTiles = mapTilesBackground;
+shuffleArray(mapTiles);
+
 const app = express();
 const server = new http.Server(app);
 const io = socketIo(server, {
-    cors: {
-        origin: '*',
-    }
+	cors: {
+		origin: '*',
+	}
 });
 
 app.use(cors());
@@ -20,17 +25,58 @@ app.use(bodyParser.json());
 
 app.use(routes);
 
-io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
-    console.log('new client connected');
-    socket.emit('connection', null);
+// middleware
+io.use((socket, next) => {
+	// let token = socket.handshake.query.token;
+	console.log('??')
+
+	// if (isValid(token)) {
+	//   return next();
+	// }
+
+	//should validate token here.
+	// if (token) {
+	return next();
+	// }
+	// return next(new Error('authentication error'));
+});
+
+// then
+io.on('connection', (socket) => {
+	let token = socket.handshake.query.token;
+	console.log(1, token)
+
+	socket.emit('game on', mapTiles)
+	// io.emit('hey')
+	// ...
 });
 
 app.listen(API_PORT, () => {
-    console.log(`Api listening on port ${API_PORT}!`);
+	console.log(`Api listening on port ${API_PORT}!`);
 });
 
 server.listen(Number(API_PORT) + 1, () => {
-    console.log(`SocketIo listening on port ${Number(API_PORT) + 1}!`);
-    // logger.info(`Socker listening on port ${API_PORT + 1}!`);
-    // logger.info(`Api and socker whitelisted for ${hosts}`);
+	console.log(`SocketIo listening on port ${Number(API_PORT) + 1}!`);
+	// logger.info(`Socker listening on port ${API_PORT + 1}!`);
+	// logger.info(`Api and socker whitelisted for ${hosts}`);
 });
+
+
+
+
+
+// io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
+//     console.log('new client connected');
+//     socket.emit('connection', null);
+// });
+
+//CUSTOM NAMESPACE BELOW 
+// const nsp = io.of('/my-namespace');
+
+// nsp.on('connection', socket => {
+  //     console.log('someone connected');
+  // });
+
+  // nsp.emit('hi', 'everyone!');
+  //CUSTOM NAMESPACE ABOVE
+
